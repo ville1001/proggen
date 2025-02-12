f196a1e95277be937676a1aeceff3419136a8e41
Ville Nilsson <villenilsson@Villes-MacBook-Air.local>
2025-02-12 16:38:36 +0100

    python O3-1 updaterad

---
+++ PythonO3-1.py
@@ -0,0 +1,31 @@
+print('=== Välkommen! Mata in ett av menyvalen. ===')
+
+def RapporteraBetyg():
+    Resultat = input(str("Mata in en kurs och ett betyg på formen kurskod:betyg: "))
+
+    while Resultat != ' ':
+        rapport = tuple(Resultat.split(':'))
+        print(rapport)
+        Resultat = input(str("Mata in en kurs och ett betyg på formen kurskod:betyg: "))
+
+    print("Klar med inmatningar")
+
+
+while True:
+    print('')
+    print("1: Mata in kurser och  betyg")
+    print("2: Gör en rapportering")
+    print("0: Avsluta")
+    choice = int(input("Mata in ett menyval: "))
+    
+
+    if(choice == 1):
+        RapporteraBetyg()
+    elif(choice == 2):
+        print("spling")
+    elif(choice == 0):
+        print("Programmet avslutas")
+        break
+    else:
+        print("Felaktigt val, försök igen")
+
