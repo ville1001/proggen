b5602b779f275d39fe9e55599aa3f98ed7533885
Ville Nilsson <villenilsson@Villes-MacBook-Air.local>
2025-02-14 16:17:40 +0100

    updaterad

--- PythonO3-1.py
+++ PythonO3-1.py
@@ -1,5 +1,7 @@
 print('=== Välkommen! Mata in ett av menyvalen. ===')
 
+
+
 def IsGradeCorrect(Grade):
     if ':' not in Grade:
         return(False)
@@ -11,18 +13,19 @@ def IsGradeCorrect(Grade):
     
 
 
-
 def RapporteraBetyg():
     Resultat = input(str("Mata in en kurs och ett betyg på formen kurskod:betyg: "))
-    while Resultat != ' ' and Resultat != "":
+    course = ()
+
+    while Resultat != ' ' and Resultat != '':
         if IsGradeCorrect(Resultat):
-            print("pling")
+            course = course + tuple(Resultat.split(':'))
 
         else:
             print("Felaktig inmatning, försök igen!")
 
         Resultat = input(str("Mata in en kurs och ett betyg på formen kurskod:betyg: "))
-    
+    #Archive(course)
     print("Klar med inmatningar")
 
 
@@ -32,8 +35,8 @@ while True:
     print("1: Mata in kurser och  betyg")
     print("2: Gör en rapportering")
     print("0: Avsluta")
+
     choice = int(input("Mata in ett menyval: "))
-    
 
     if(choice == 1):
         RapporteraBetyg()
