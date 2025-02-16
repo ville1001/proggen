8248332e37650c6c405d28ac2d0ccfc336ac7908
Ville Nilsson <villenilsson@Villes-MacBook-Air.local>
2025-02-16 15:03:08 +0100

    updaterad

--- PythonO3-1.py
+++ PythonO3-1.py
@@ -1,6 +1,39 @@
 print('=== Välkommen! Mata in ett av menyvalen. ===')
 
 
+def IsDateCorrect(Date):
+    try:
+        year, month, day = map(int, Date.split('-'))  # Omvandlar till int
+
+        if year < 0 or year > 3000:
+            print("Felaktigt år")
+            return False #kontrolerar året
+        elif month < 1 or month > 12:
+            print("Felaktigt månad")
+            return False #kontrollerar månaden
+        elif day < 1 or day > 31:
+            print("Felaktigt dag")
+            return False #Kontrollerar dagen
+        else:
+            return(True)
+    except:
+        print("Value error exception")
+        return(False)
+
+
+
+def lagra(Grades):
+    Date = input("Mata in ett rapporteringdatum: ")
+
+    if(IsDateCorrect(Date)):
+        for i in range(len(Grades)):
+            Grades[i] = Grades[i] + (Date,)
+
+    else:
+        print("ojdå fel inmatning, försök igen")
+        lagra(Grades)
+
+    print(Grades)
 
 def IsGradeCorrect(Grade):
     if ':' not in Grade:
@@ -12,25 +45,24 @@ def IsGradeCorrect(Grade):
         return(False)
     
 
-
 def RapporteraBetyg():
     Resultat = input(str("Mata in en kurs och ett betyg på formen kurskod:betyg: "))
-    course = ()
+    Archive = []
 
     while Resultat != ' ' and Resultat != '':
         if IsGradeCorrect(Resultat):
-            course = course + tuple(Resultat.split(':'))
-
+            kurs_tuple = tuple(Resultat.split(':'))
+            Archive.append(kurs_tuple)
         else:
             print("Felaktig inmatning, försök igen!")
 
         Resultat = input(str("Mata in en kurs och ett betyg på formen kurskod:betyg: "))
-    #Archive(course)
     print("Klar med inmatningar")
-
+    return(Archive)
 
 
 while True:
+    
     print('')
     print("1: Mata in kurser och  betyg")
     print("2: Gör en rapportering")
@@ -39,9 +71,11 @@ while True:
     choice = int(input("Mata in ett menyval: "))
 
     if(choice == 1):
-        RapporteraBetyg()
+        Resultat = RapporteraBetyg()
+        
     elif(choice == 2):
-        print("spling")
+        lagra(Resultat)
+        
     elif(choice == 0):
         print("Programmet avslutas")
         break
